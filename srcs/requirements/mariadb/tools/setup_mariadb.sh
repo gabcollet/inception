75cfb73475493872b567b59b6ec961e2c72c4a16
gabcollet <gab_collet@hotmail.com>
2022-07-06 15:07:48 -0400

    wordpress open!

--- srcs/requirements/mariadb/tools/setup_mariadb.sh
+++ srcs/requirements/mariadb/tools/setup_mariadb.sh
@@ -14,7 +14,8 @@ else
   echo "FLUSH PRIVILEGES;" > /tmp/init.sql
   echo "ALTER USER root@localhost IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> /tmp/init.sql
   echo "CREATE DATABASE $MYSQL_DATABASE;" >> /tmp/init.sql
-  echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost' identified by '$MYSQL_PASSWORD';" >> /tmp/init.sql
+  echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> /tmp/init.sql
+  echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' identified by '$MYSQL_PASSWORD';" >> /tmp/init.sql
   echo "FLUSH PRIVILEGES;" >> /tmp/init.sql
   echo "EXIT;" >> /tmp/init.sql
 
--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,31 +1,58 @@
 #!/usr/bin/env bash
 
-# while ! /tmp/wait-for-it.sh $WORDPRESS_DB_HOST "-s" "--timeout=50"; do
-#   sleep 2
-# done
+if [ ! -d "/var/www/html" ]; then
+    mkdir -p /var/www/html;
+fi
+cd /var/www/html;
+if [ -f "index.php" ]; then
+    rm index.php;
+fi
+chmod -R 777 /var/www/html;
+touch /var/www/html/index.php;
+echo "<?php phpinfo(); ?>" >> /var/www/html/index.php;
 
-# sed -i "s/WP_DB_NAME/$WP_DB_NAME/g" /tmp/wp-config.php
-# sed -i "s/WP_DB_USER/$WP_DB_USER/g" /tmp/wp-config.php
-# sed -i "s/WP_DB_PASSWORD/$WP_DB_PASSWORD/g" /tmp/wp-config.php
-# sed -i "s/WP_DB_HOST/$WP_DB_HOST/g" /tmp/wp-config.php
-# sed -i "s/WP_TABLE_PREFIX/$WP_TABLE_PREFIX/g" /tmp/wp-config.php
+if [ ! -d "var/run/php/" ]; then
+    mkdir -p var/run/php/;
+fi
+addgroup -g 99 nginx;
+adduser -u 99 -G nginx -D nginx;
 
-# FILE_HEALTH_CHECK=/var/www/html/index.php
+#CONFIGURATION FOR WORDPRESS
+sleep 2;
 
-# cp -f /tmp/wp-config.php /var/www/html/wp-config.php
+cd /var/www/
+tar -xzf ./wordpress-6.0.tar.gz
+rm ./wordpress-6.0.tar.gz
+mv wordpress//* html/
+rm -rf wordpress
+wp core download;
+sleep 2;
 
-# if [ ! -f "$FILE_HEALTH_CHECK" ]; then
-#   # Download Wordpress into a temp directory, untar it and move it
-#   wget https://wordpress.org/latest.tar.gz -P /tmp &> /dev/null
-#   tar -xzvf /tmp/latest.tar.gz -C /var/www/html &> /dev/null
-#   mv /var/www/html/wordpress/* /var/www/html
-#   rm -rf /var/www/html/wordpress
 
-#   cd /var/www/html
-#   wp core install --url=localhost --title='Init' \
-#                   --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email='gcollet@fake.com' &> /dev/null
-#   wp user create $WP_GUEST eval@fake.com --role=editor --user_pass=$WP_GUEST_PASSWORD
-#   echo "Wordpress successfully installed and initialized"
-# fi
+wp config create --allow-root \
+                --dbname=$MYSQL_DATABASE \
+                --dbuser=$MYSQL_USER \
+                --dbpass=$MYSQL_PASSWORD \
+                --dbhost=$WP_DB_HOST \
+                --dbcharset="utf8" \
+                --dbcollate="utf8_general_ci" \
+                --path="/var/www/html"
+wp core install --allow-root \
+                --title="Wordpress" \
+                --admin_name="${MYSQL_USER}" \
+                --admin_password="${MYSQL_PASSWORD}" \
+                --admin_email="${MYSQL_EMAIL}" \
+                --skip-email \
+                --url="${DOMAIN_NAME}" \
+                --path="/var/www/html"
+wp user create --allow-root \
+                $WP_DB_USER \
+                $WP_EMAIL \
+                --role=author \
+                --user_pass=$WP_DB_PASSWORD \
+                --path="/var/www/html"
+
+
+echo "WORDPRESS INSTALLED SUCCESSFULLY"
 
 exec "$@"
